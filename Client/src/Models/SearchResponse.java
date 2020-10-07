e31df85bebd854074bacaf54353369eeebd7da38
DESKTOP-Q0VFM82\phung <phunghathao1999@gmail.com>
2020-10-07 14:52:40 +0700

    done

---
+++ Client/src/Models/SearchResponse.java
@@ -0,0 +1,24 @@
+package Models;
+
+import java.util.List;
+
+public class SearchResponse extends BaseResponse {
+    private List<String> relatedKeyword;
+
+    public SearchResponse(int responseCode) {
+        super(responseCode);
+    }
+
+    public List<String> getRelatedKeyword() {
+        return relatedKeyword;
+    }
+
+    public void setRelatedKeyword(List<String> relatedKeyword) {
+        this.relatedKeyword = relatedKeyword;
+    }
+
+    @Override
+    protected String setResponseType() {
+        return this.getClass().getName();
+    }
+}
