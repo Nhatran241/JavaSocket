53ebf49735601f5200bbe0ce84192aa61d32d099
hieuit123 <43605814+hieuit123@users.noreply.github.com>
2020-10-14 16:54:06 +0700

    server: Add class RelatedTopicRequest

--- servers/src/server/library/model/request/RelatedTopicRequest.java
+++ servers/src/server/library/model/request/RelatedTopicRequest.java
@@ -1,8 +1,46 @@
 package server.library.model.request;
 
 public class RelatedTopicRequest extends BaseRequest {
+    public static String apiKeyGoogle = "AIzaSyByAfwF3d7h79pzRcxiz8c6fqW-0PK6Avo";
+    public static String idSearchEngine = "e34885c89b9f0563b";
+
+    private String relatedTopicQuery;
+    private int pageNumber;
+
+    public RelatedTopicRequest(){
+        super();
+        pageNumber = 0;
+    }
+
     @Override
     public String setRequestType() {
         return this.getClass().getName();
     }
+
+    public String getRelatedTopicQuery() {
+        return relatedTopicQuery;
+    }
+
+    public void setRelatedTopicQuery(String relatedTopicQuery) {
+        this.relatedTopicQuery = relatedTopicQuery;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "RelatedTopicRequest{" +
+                "relatedTopicQuery='" + relatedTopicQuery + '\'' +
+                ","+idSearchEngine+
+                ","+apiKeyGoogle+
+                ","+pageNumber+
+                '}';
+    }
 }
+
